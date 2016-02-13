6105cfb36932671b651cf54fefa776b857a8840e
sisisin <azsisisin@gmail.com>
2016-02-13 13:44:59 +0900

    scheme test

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 import StoneSkin from 'stone-skin/with-tv4';
 import 'babel-polyfill';
 
-class ItemStore extends StoneSkin.IndexedDb {
+class ServiceStore extends StoneSkin.IndexedDb {
   constructor(){
     super();
-    this.storeName = 'Item';
+    this.storeName = 'Service';
     this.schema = {
       properties: {
-        title: {
-          type: 'string'
-        }
+        title: { type: 'string' }
+        , user: { type: 'string' }
+        , pass: { type: 'string' }
+        , mail: { type: 'string' }
       }
     }
   }
 }
 
-let itemStore = new ItemStore();
+let itemStore = new ServiceStore();
 (async function() {
   await itemStore.ready;
-  await itemStore.save({title: 'foo', _id: '1'});
-  let item = await itemStore.find('xxx');
-  console.log(item);
+  await itemStore.save({title: 'foo', _id: '1', userName:'+hub', userPass:'abe'});
+  let item = await itemStore.find('1');
+  console.log(item.userName);
   let items = await itemStore.all();
   console.log(items);
 })();
